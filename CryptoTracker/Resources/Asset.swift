f479f49a48ccd4238337351516627c878d4fa7a4
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-23 01:39:12 +0530

    Add: Assets

---
+++ CryptoTracker/Resources/Asset.swift
@@ -0,0 +1,16 @@
+//
+//  Asset.swift
+//  CryptoTracker
+//
+//  Created by Avijeet on 23/02/24.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Assets
+enum Asset {
+    static var coin: UIImage { return UIImage(named: "coin")! }
+    static var inactive: UIImage { return UIImage(named: "inactive")! }
+    static var token: UIImage { return UIImage(named: "token")! }
+}
