014c224fb653107c84bd21a82e28ef915345c05e
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-23 01:38:55 +0530

    Add: networking support

---
+++ CryptoTracker/Networking/ApiPath.swift
@@ -0,0 +1,13 @@
+//
+//  ApiPath.swift
+//  CryptoTracker
+//
+//  Created by Avijeet on 23/02/24.
+//
+
+import Foundation
+
+// MARK: - ApiPath
+enum ApiPath {
+    static let cryptoCurrencyUrl: NetworkRequestObject = .init(url: "https://run.mocky.io/v3/ac7d7699-a7f7-488b-9386-90d1a60c4a4b")
+}
