c98f1e83145d561294413506328f1247842bb0d3
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-22 22:36:13 +0530

    Remove: Call from controller

--- CryptoTracker/Controllers/HomeViewController.swift
+++ CryptoTracker/Controllers/HomeViewController.swift
@@ -12,16 +12,5 @@ import UIKit
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let apiManager = NetworkClient.shared
-        
-        apiManager.sendRequest(urlString: "https://run.mocky.io/v3/ac7d7699-a7f7-488b-9386-90d1a60c4a4b", method: .get) { (response: Result<CryptoCurrenciesModel, NetworkError>) in
-            switch response {
-            case .success(let data):
-                print(data)
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
 }
