c5e912ffe951ad0f0d559856e771ec5b40af60f9
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-22 21:26:01 +0530

    Add: Application icon and launchscreen

--- CryptoTracker/Controllers/HomeViewController.swift
+++ CryptoTracker/Controllers/HomeViewController.swift
@@ -1,19 +1,16 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CryptoTracker
 //
 //  Created by Avijeet on 22/02/24.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+// MARK: - HomeViewController
+class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
